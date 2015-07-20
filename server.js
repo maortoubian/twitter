64d058b6b776fe1da4011510c57c4b4e0925c4dd
maortoubian <maortoubian@gmail.com>
2015-07-21 00:39:48 +0300

    first try

---
+++ server.js
@@ -0,0 +1,54 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: 'mtEsDDynLBPI0bk0QvOjvn39n',
+  consumer_secret: 't5aFQ755S4CVv4c2cUcrP7ZegOmtAW3oxnvOSl7EmXXZfU0KXP',
+  access_token_key: '3247461185-ArvrlKE3ZCInVfsgFjp0rWpYBHrNXHLGwPZUhb1',
+  access_token_secret: 'Rt4pieKZUv6zsmoUnVLmWQm5pgm0sWnVctf95uyJ0lM52'
+});
+
+
+client.stream('statuses/filter', {track: 'maortoubianibm'},  function(stream){
+
+	var msg = '';
+
+	stream.on('data', function(tweet) {
+
+	   		msg = randomMsg();
+	   		console.log(msg);
+
+	   		client.post('statuses/update', { in_reply_to_status_id: tweet.id_str, status: 'Hi @' + tweet.user.screen_name + msg }, function (err, data, res) {});
+
+	});
+
+ 	stream.on('error', function(error) {
+	    	console.log(error);
+	});
+
+
+ });
+
+
+ function randomMsg () {
+
+		var randomNumber = Math.floor((Math.random() * 3) + 0)
+
+		var msg1 = ' thank you for mentioning me in your Tweet';
+		var msg2 = ' thanks!';
+		var msg3 = ' thank you :)';
+
+		msgArray = [msg1,msg2,msg3];
+
+		return msgArray[randomNumber];
+ }
+
+
+
+
+
+//---------------------------------------------tweet-----------------------------------------
+// client.post('statuses/update', {status: 'I am a tweet'}, function(error, tweet, response){
+//   if (!error) {
+//     console.log(tweet);
+//   }
+// });
